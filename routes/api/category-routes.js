1fd333d66391c4b2c4143549dd465f0724f08094
Megan Steblay <megan@steblay.net>
2021-07-14 20:31:42 -0500

    Fixing the GET, POST, PUT, and DELETE for the category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,9 +5,11 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      // include associated Products
+      include: [{ model: Product }],
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
@@ -16,11 +18,10 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      // JOIN with locations, using the Trip through table
-      include: [{ model: Product, through: Product, as: 'category_products' }]
+      // include associated Products
+      include: [{ model: Product }]
     });
 
     if (!categoryData) {
@@ -46,6 +47,24 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+    // Calls the update method on the Book model
+    Category.update(
+      {
+        // All the fields you can update and the data attached to the request body.
+        category_name: req.body.category_name,
+      },
+      {
+        // Gets the books based on the id given in the request parameters
+        where: {
+          id: req.params.id,
+        },
+      }
+    )
+      .then((updatedCategory) => {
+        // Sends the updated category as a json response
+        res.json(updatedCategory);
+      })
+      .catch((err) => res.json(err));
 });
 
 router.delete('/:id', async (req, res) => {
