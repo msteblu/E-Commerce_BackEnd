021aff4c9ec304574dca9e1e1d6d20cb5977d2ec
Megan Steblay <megan@steblay.net>
2021-07-14 21:34:45 -0500

    Updating GET to find a product by its ID so that it is connected to Category and Tag

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -23,11 +23,11 @@ router.get('/', async (req, res) => {
 // get one product
 router.get('/:id', async (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, through: ProductTag, as: 'producttag_category' },
-                { model: Tag, through: ProductTag, as: producttag_tag}]
+      // be sure to include its associated Category and Tag data
+      include: [{ model: Category },
+                { model: Tag, through: ProductTag, as: 'product_multipletags' }]
     });
 
     if (!productData) {
