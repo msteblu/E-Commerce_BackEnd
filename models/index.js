d4a92269dbe0beb8f7f3d6a8eb8731fdefacac93
Megan Steblay <megan@steblay.net>
2021-07-14 22:23:21 -0500

    Fixing foreign key relationships

--- models/index.js
+++ models/index.js
@@ -18,6 +18,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   // Define the third table needed to store the foreign keys
+  foreignKey: 'product_id',
   through: {
     model: ProductTag,
     unique: false
@@ -29,6 +30,7 @@ Product.belongsToMany(Tag, {
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   // Define the third table needed to store the foreign keys
+  foreignKey: 'tag_id',
   through: {
     model: ProductTag,
     unique: false
